77bd0a3916624e866e3a6b1226800d0d56f18db2
jenilmodi1813 <jenilmodi1813@gmail.com>
2025-12-16 21:21:07 +0530

    create auth-service

---
+++ auth-service/src/test/java/com/jira/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
