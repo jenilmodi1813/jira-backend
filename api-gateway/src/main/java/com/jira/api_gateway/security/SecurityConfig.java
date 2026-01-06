0c633a12defa8c6f1c2a48aa1936135d75662c8a
jenilmodi1813 <jenilmodi1813@gmail.com>
2026-01-06 16:14:45 +0530

    Fixing cros configration

--- api-gateway/src/main/java/com/jira/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/jira/api_gateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jira.api_gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -10,12 +11,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
-
         return http
+                .cors(cors -> {})   //
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .pathMatchers("/api/auth/**").permitAll()
-                        .anyExchange().permitAll() // IMPORTANT
+                        .anyExchange().permitAll()
                 )
                 .build();
     }
